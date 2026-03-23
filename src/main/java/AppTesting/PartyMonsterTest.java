2291c30825a30801a3a8223280a70f2e73774034
Yingyingcheng <doris82717@gmail.com>
2026-03-22 21:52:34 -0700

    move file to src/main/java/AppTesting

--- src/main/java/AppTesting/PartyMonsterTest.java
+++ src/main/java/AppTesting/PartyMonsterTest.java
@@ -3,6 +3,7 @@ package AppTesting;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import java.time.Duration;
 
 public class PartyMonsterTest {
